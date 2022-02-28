3806c198d44f660ce858186381b547b245488257
quangnt0107 <quangnt0107@gmail.com>
2022-02-28 15:10:21 +0700

    initialize main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const yargs = require("yargs");
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Enter your address",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h")
+  .argv;
+
+const address = argv.address;
